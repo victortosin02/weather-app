0b744b5140e0316f94d74e4df1ec752a8671d4d4
Victor <victortosin01@gmail.com>
2024-09-03 10:01:50 +0100

    New Commit

--- app/api/uv/route.ts
+++ app/api/uv/route.ts
@@ -3,22 +3,32 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   try {
     const searchParams = req.nextUrl.searchParams;
-
     const lat = searchParams.get("lat");
     const lon = searchParams.get("lon");
 
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=uv_index_max,uv_index_clear_sky_max&timezone=auto&forecast_days=1`;
+    // Get the UV Index URL from environment variables
+    const uvIndexUrl = process.env.UV_INDEX_URL;
+
+    if (!uvIndexUrl) {
+      throw new Error("UV_INDEX_URL is not defined in environment variables.");
+    }
+
+    const url = `${uvIndexUrl}?latitude=${lat}&longitude=${lon}&daily=uv_index_max,uv_index_clear_sky_max&timezone=auto&forecast_days=1`;
 
     const res = await fetch(url, {
       next: { revalidate: 900 },
     });
 
+    if (!res.ok) {
+      throw new Error("Network response was not ok.");
+    }
+
     const uvData = await res.json();
 
     return NextResponse.json(uvData);
   } catch (error) {
-    console.log("Error Getting Uv Data");
+    console.error("Error Getting UV Data", error);
 
-    return new Response("Error getting Uv Data", { status: 500 });
+    return new Response("Error getting UV Data", { status: 500 });
   }
 }
